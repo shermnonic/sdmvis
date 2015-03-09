22d93f07c5c92d2425e4909ee51d789f2d76ca0e
386dx25 <386dx25@716a8cc4-bca1-46f2-b623-109a26fac70c>
2015-03-09 14:58:56 +0000

    slvis: Geometry shader with line strip output.

--- trunk/slvis/shader/streamline.gs.glsl
+++ trunk/slvis/shader/streamline.gs.glsl
@@ -2,15 +2,24 @@
 #version 150
 
 layout(points) in;
-layout(points, max_vertices=1) out;
+layout(line_strip, max_vertices=5) out;
 
 //uniform sampler3D voltex;
-//uniform sampler3D warpfield;
+uniform sampler3D warpfield;
 //uniform float     isovalue;
 
 void main()
 {
-	gl_Position = gl_in[0].gl_Position;
-	EmitVertex();
+	//gl_Position = gl_in[0].gl_Position;
+	//EmitVertex();
+	
+	int i;
+	for( i=0; i < 5; i++ )
+	{	
+		float d = float(i) / 4.0;
+		gl_Position = gl_in[0].gl_Position + 20.0*d*vec4(1.0,1.0,1.0,0.0);
+		EmitVertex();
+	}
+	
 	EndPrimitive();	
 }
